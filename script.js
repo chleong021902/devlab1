dd0b1e9ddf3915bfddd777aa49b8873e734785d5
Chi Chon Leong <clamer2016.cms.hs@gmail.com>
2018-06-12 21:26:40 +0000

    change

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 /* global $ */
-$(document).ready(function ({
-    $("about").click(function ({
-        location.href="about-me.html"
+$(document).ready(function (){
+    $("#about").click(function (){
+        //.href="about-me.html"
+        window.location.href="about-me.html";
+    });    
+    
+    $("#portfolio").click(function (){
+        //.href="about-me.html"
+        window.location.href="port.html";
     });
 
 });
